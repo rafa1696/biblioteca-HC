7e3a83d46c0e90657b38731b4b6f1abfb272c5c0
rafa1696 <79325328+rafa1696@users.noreply.github.com>
2021-07-06 16:31:41 -0300

    Iteração inicial do exercício do Hiring Coders

---
+++ database.js
@@ -0,0 +1,63 @@
+const livros = [{
+        id: 1,
+        nome: "Digital Minimalism",
+        autor: "Cal Newport",
+        categoria: "Produtividade e estilo de vida",
+        paginas: 254,
+        recomenda: false,
+        leu: false
+    },
+    {
+        id: 2,
+        nome: "Quem Era Ela",
+        autor: "JP Delaney",
+        categoria: "Thriller",
+        paginas: 351,
+        recomenda: false,
+        leu: true
+
+    },
+    {
+        id: 3,
+        nome: "Como funciona o fascismo",
+        autor: "Jason Stanley",
+        categoria: "História e Teoria Política",
+        paginas: 173,
+        recomenda: true,
+        leu: true
+
+    }, 
+    {
+        id: 4,
+        nome: "Objetos Cortantes",
+        autor: "Gillian Flynn",
+        categoria: "Thriller",
+        paginas: 242,
+        recomenda: true,
+        leu: true
+
+    },
+    {
+        id: 5,
+        nome: "Lugares Escuros",
+        autor: "Gillian Flynn",
+        categoria: "Thriller",
+        paginas: 384,
+        recomenda: true,
+        leu: true
+
+    },
+    {
+        id: 6,
+        nome: "Garota Exemplar",
+        autor: "Gillian Flynn",
+        categoria: "Thriller",
+        paginas: 494,
+        recomenda: true,
+        leu: true
+
+    }
+
+]
+
+module.exports = livros;
